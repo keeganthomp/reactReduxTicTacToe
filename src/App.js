20310a0f910d1e2874fea3ce19e2d6b15816da5c
keeganthomp <whereiskeegan@gmail.com>
2017-08-02 10:06:58 -0500

    getting board creted

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="board"> 
+          <div className="playingSquare" id="1"></div>
+          <div className="playingSquare" id="2"></div>
+          <div className="playingSquare" id="3"></div>
+          <div className="playingSquare" id="4"></div>
+          <div className="playingSquare" id="5"></div>
+          <div className="playingSquare" id="6"></div>
+          <div className="playingSquare" id="7"></div>
+          <div className="playingSquare" id="8"></div>
+          <div className="playingSquare" id="9"></div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
